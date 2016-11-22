42f84cc6173fdfa3ca835d682dbceb49af939930
Leonard Tomczak <Leonard@Leonard.fritz.box>
2016-11-22 21:10:21 +0100

    enterprise model fix, camera script

---
+++ Assets/Star Trek/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public Vector3 offset = new Vector3 (0, 0, 0);
+	public GameObject player;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + offset;
+
+		Quaternion idealRotation = Quaternion.LookRotation(player.transform.position-transform.position);
+		transform.rotation = Quaternion.Slerp(transform.rotation, idealRotation, Time.timeScale);
+	}
+}
