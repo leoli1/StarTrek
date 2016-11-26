776c3702384d82b4ce0f37d74945dbc57704b3ea
Leonard Tomczak <Leonard@Leonard.fritz.box>
2016-11-26 19:26:53 +0100

    player movement, skybox, meshcollider-rigidbody fix

--- Assets/Star Trek/Scripts/CameraScript.cs
+++ Assets/Star Trek/Scripts/CameraScript.cs
@@ -3,18 +3,37 @@ using System.Collections;
 
 public class CameraScript : MonoBehaviour {
 
-	public Vector3 offset = new Vector3 (0, 0, 0);
-	public GameObject player;
+	//public Vector3 offset = new Vector3 (0, 0, 0);
+	public float camera_ortho_size = 30;
+	public float camera_angle = 30;
+	//public GameObject player;
+
+	public GameObject camera_center;
+
+	Camera camera;
 
 	void Start () {
+		camera = this.GetComponent<Camera> ();
+		camera.orthographicSize = camera_ortho_size;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = player.transform.position + offset;
+		camera_center.transform.position = Player.player.transform.position;
+		Vector3 r = camera_center.transform.localRotation.eulerAngles;
+		Quaternion new_r = Quaternion.identity;
+		new_r.eulerAngles = new Vector3 (30, r.y, r.z);
+		camera_center.transform.localRotation = new_r;
+		if (camera.orthographic) {
+			transform.localPosition = new Vector3 (0, 0, this.camera_ortho_size * (-3));
+		} else {
+			transform.localPosition = new Vector3 (0, 0, -100);
+		}
+
+		/*transform.position = player.transform.position + offset;
 
 		Quaternion idealRotation = Quaternion.LookRotation(player.transform.position-transform.position);
-		transform.rotation = Quaternion.Slerp(transform.rotation, idealRotation, Time.timeScale);
+		transform.rotation = Quaternion.Slerp(transform.rotation, idealRotation, Time.timeScale);*/
 	}
 }
---
+++ Assets/Star Trek/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public static GameObject player;
+
+	public float velocity;
+	public float accel = 2;
+
+	public float rotation_speed = 20;
+
+	Rigidbody rigidbody;
+
+	void Start () {
+		Player.player = gameObject;
+		rigidbody = this.GetComponent<Rigidbody> ();
+		if (rigidbody == null) {
+			gameObject.AddComponent<Rigidbody> ();
+			rigidbody = this.GetComponent<Rigidbody> ();
+			rigidbody.useGravity = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velo_input ();
+		rigidbody.velocity = velocity*transform.forward*5;
+		rot_input ();
+	}
+
+	void rot_input() {
+		if (Input.GetKey (KeyCode.D)) {
+			this.transform.Rotate (new Vector3 (0, rotation_speed * Time.deltaTime, 0));
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			this.transform.Rotate (new Vector3 (0, -rotation_speed * Time.deltaTime, 0));
+		}
+	}
+
+	void velo_input(){
+		if (Input.GetKey (KeyCode.W)) {
+			velocity += accel*Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			velocity -= accel*Time.deltaTime;
+		}
+		velocity = Mathf.Clamp (velocity, -1, 4);
+	}
+}
